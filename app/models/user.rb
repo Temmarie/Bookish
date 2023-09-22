8ec40450836bda349c52f577cec7dc4f3d4c2964
Temmarie <temmar_whyte16@yahoo.com>
2023-09-22 09:53:35 +0100

    Generate devise controller for users and views. Add images to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,22 @@ class User < ApplicationRecord
    scope :all_except, ->(user) { where.not(id: user) }
    after_create_commit { broadcast_append_to "users" }
    has_many :messages 
+   has_one_attached :avatar
+
+   after_commit :add_default_avatar, on: %i[create update]
+
+   private
+
+   def add_default_avatar
+    return if avatar.attached?
+      
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_avatar.avif'
+          )
+        ), filename: 'default_avatar.avif',
+        content_type: 'image/avif'
+      )
+   end
 end
