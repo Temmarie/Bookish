31d004a2b214bf40eeafe464ab18f0b503e87732
Temmarie <temmar_whyte16@yahoo.com>
2023-09-22 08:52:39 +0100

    Successfully implement direct messaging between users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,14 @@ class UsersController < ApplicationController
     @single_room = Room.where(name: @room_name).first || Room.create_private_room([@user, current_user], @room_name)
 
     @message = Message.new
-    @messages = @single_room.messages.order(:created_at: asc)
-
+    @messages = @single_room.messages.order(created_at: :asc)
     render 'rooms/index'
   end
 
   private
 
   def get_name(user1, user2)
-    user = [user1,user2].sort
-    "private_#{user[0].id}_ #{user[1].id}"
+    user = [user1, user2].sort
+    "private_#{user[0].id}_#{user[1].id}"
   end
-
-  def
 end
