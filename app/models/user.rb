1901f29a666a6cec2e5d096307450c1b5331a37a
Temmarie <temmar_whyte16@yahoo.com>
2023-09-23 21:26:12 +0100

    Add user status

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,33 +3,29 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-   scope :all_except, ->(user) { where.not(id: user) }
-   after_create_commit { broadcast_append_to "users" }
-    after_update_commit { broadcast_update }
-
-   has_many :messages 
-   has_one_attached :avatar
+  scope :all_except, ->(user) { where.not(id: user) }
+  after_create_commit { broadcast_append_to 'users' }
+  after_update_commit { broadcast_update }
+  has_many :messages
+  has_one_attached :avatar
 
   enum status: %i[offline away online]
 
-   after_commit :add_default_avatar, on: %i[create update]
+  after_commit :add_default_avatar, on: %i[create update]
 
-   def avatar_thumbnail
-    avatar.variant(resize_to_limit:[150, 150]).processed
-   end
+  def avatar_thumbnail
+    avatar.variant(resize_to_limit: [150, 150]).processed
+  end
 
-   def chat_avatar
-    avatar.variant(resize_to_limit:[50, 50]).processed
-   end
+  def chat_avatar
+    avatar.variant(resize_to_limit: [50, 50]).processed
+  end
 
-
-   def broadcast_update
+  def broadcast_update
     broadcast_replace_to 'user_status', partial: 'users/status', user: self
-   end
+  end
 
-
-   def status_to_css
+  def status_to_css
     case status
     when 'online'
       'bg-success'
@@ -42,18 +38,15 @@ class User < ApplicationRecord
     end
   end
 
-   private
+  private
 
-   def add_default_avatar
+  def add_default_avatar
     return if avatar.attached?
-      
-      avatar.attach(
-        io: File.open(
-          Rails.root.join(
-            'app', 'assets', 'images', 'default_avatar.jpg'
-          )
-        ), filename: 'default_avatar.jpg',
-        content_type: 'image/jpg'
-      )
-   end
+
+    avatar.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_avatar.jpg')),
+      filename: 'default_avatar.jpg',
+      content_type: 'image/jpg'
+    )
+  end
 end
