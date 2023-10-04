3786c35b5b50190df98e33c932b39cff5c34c213
Temmarie <temmar_whyte16@yahoo.com>
2023-10-04 14:22:16 +0100

    Add message previews, image, audio and file uploads

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,21 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
-  after_create_commit {broadcast_append_to self.room}
+  after_create_commit { broadcast_append_to room }
   before_create :confirm_participant
+  has_many_attached :attachments, dependent: :destroy
 
+  def chat_attachment(index)
+    target = attachments[index]
+    return unless attachments.attached?
 
+    if target.image?
+      target.variant(resize_to_limit: [150, 150]).processed
+    elsif target.video?
+      target.variant(resize_to_limit: [150, 150]).processed
+    end
+  end
+  
   def confirm_participant
     return unless room.is_private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-
   resources :rooms do
     resources :messages
   end
+
   root 'home#index'
-  devise_for :users, controllers: {
+
+devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get 'user/:id', to: 'users#show', as: 'user'
