67feb63e7c7f756df57a7cc9d3f00a9a6b34f53a
Temmarie <temmar_whyte16@yahoo.com>
2023-10-04 10:25:03 +0100

    Add user statuses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
+  after_action :set_status
   def index
   end
 
   def chats
+  end
+
+  private 
+
+  def set_status
+    current_user.update!(status: User.statuses[:offline]) if current_user
   end
 end
