7d9baab835cffc7f941928812717e5ddc9927487
Temmarie <temmar_whyte16@yahoo.com>
2023-10-14 16:40:48 +0100

    Add last seen message to chats and rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,10 +2,9 @@ class RoomsController < ApplicationController
   include RoomsHelper
   before_action :authenticate_user!
   before_action :set_status
-
   def index
     @room = Room.new
-    @joined_rooms = current_user.joined_rooms
+    @joined_rooms = current_user.joined_rooms.order('last_message_at DESC')
     @rooms = search_rooms
 
     @users = User.all_except(current_user)
@@ -17,11 +16,11 @@ class RoomsController < ApplicationController
 
     @room = Room.new
     @rooms = search_rooms
-    @joined_rooms = current_user.joined_rooms
-  
+    @joined_rooms = current_user.joined_rooms.order('last_message_at DESC')
 
     @message = Message.new
-    pagy_messages =  @single_room.messages.includes(:user).order(created_at: :desc)
+
+    pagy_messages = @single_room.messages.includes(:user).order(created_at: :desc)
     @pagy, messages = pagy(pagy_messages, items: 10)
     @messages = messages.reverse
 
@@ -59,10 +58,9 @@ class RoomsController < ApplicationController
     redirect_to rooms_path
   end
 
-
   private
 
   def set_status
     current_user.update!(status: User.statuses[:online]) if current_user
   end
-end 
+end
