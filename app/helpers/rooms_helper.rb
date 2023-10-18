8181e1e3460f53e29782bc59ce8414c18526de70
Temmarie <temmar_whyte16@yahoo.com>
2023-10-18 20:38:44 +0100

    Add sorting algorithm for chatrooms and direct messages

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -10,6 +10,10 @@ module RoomsHelper
         []
       end
     end
+    def get_name(user1, user2)
+      user = [user1, user2].sort
+      "private_#{user[0].id}_#{user[1].id}"
+    end
   end
 
   ## change LIKE to ILIKE after fixing the pg problem
